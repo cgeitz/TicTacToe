3cac194b620d1f23ad5681245f1c5f8a6f5c15ec
Chris Geitz <ccgeitz@comcast.net>
2014-03-25 15:00:23 -0500

    added top row win alert function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var choices = [false, false, false, false, false, false, false, false, false];
+
 $(document).ready(function(){
 	$('.item').click(function(){
 		$(this).addClass('obama');
@@ -6,3 +8,23 @@ $(document).ready(function(){
 		$(this).toggleClass('kanye')
 	});
 });
+
+('container').onclick=function(){
+	if($('.box0').hasClass('obama')){
+		choices[0]= true;
+	}
+	else if($('.box1').hasClass('obama')){
+		choices[1]= true;
+	}
+	else if($('.box2').hasClass('obama')){
+		choices[2]= true;
+	};
+
+if(choices[0]===true && choices[1]===true && choices[2]===true){
+alert("obama wins!");
+};
+};
+
+
+
+
